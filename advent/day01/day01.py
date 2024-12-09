f5f4045b9f412eb712d6578be1c92e6ad8d9e5d0
Alexander Konstantinov <konstantinov@gmail.com>
2024-12-09 17:27:33 +0000

    Simplify Day 1

--- advent/day01/day01.py
+++ advent/day01/day01.py
@@ -9,46 +9,43 @@ def read_input(path: str) -> tuple[list[int], list[int]]:
 
     with open(path) as f:
         for line in f:
-            line = line.strip()
-            if not line:
-                continue
-            numbers = list(filter(None, line.split(" ")))
-            if len(numbers) != 2:
-                print(f"Warning: unexpected line\n> {line}")
-                continue
-            list1.append(int(numbers[0]))
-            list2.append(int(numbers[1]))
+            a, b = list(filter(None, line.strip().split(" ")))
+            list1.append(int(a))
+            list2.append(int(b))
     return sorted(list1), sorted(list2)
+
+
+def get_sum_distances(list1: list[int], list2: list[int]) -> int:
+    return sum(abs(b - a) for a, b in zip(list1, list2))
+
+
+def get_similarity_score(list1: list[int], list2: list[int]) -> int:
+    n = len(list1)
+
+    similarity = 0
+    i = 0
+    prev_value = None
+    occurrences = 0
+    for value in list1:
+        if prev_value is None or prev_value != value:
+            occurrences = 0
+            while i < n and list2[i] < value:
+                i += 1
+            while i < n and list2[i] == value:
+                occurrences += 1
+                i += 1
+        prev_value = value
+        similarity += value * occurrences
+    return similarity
 
 
 def run(input_path: str) -> None:
     list1, list2 = read_input(input_path)
 
-    n = len(list1)
-
-    sum_distances = 0
-    for i in range(n):
-        a, b = list1[i], list2[i]
-        distance = abs(b - a)
-        sum_distances += distance
-
+    sum_distances = get_sum_distances(list1, list2)
     print(f"Total distance = {sum_distances}")
 
-    similarity = 0
-    j, prev_j = 0, 0
-    prev_value = None
-    occurrences = 0
-    for i, value in enumerate(list1):
-        if prev_value is None or prev_value != value:
-            occurrences = 0
-            while j < n and list2[j] < value:
-                j += 1
-            while j < n and list2[j] == value:
-                occurrences += 1
-                j += 1
-        prev_j, prev_value = j, value
-        similarity += value * occurrences
-
+    similarity = get_similarity_score(list1, list2)
     print(f"Similarity score = {similarity}")
 
 
